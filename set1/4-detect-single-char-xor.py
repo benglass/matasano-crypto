f32fa57d42ef8e26eb5362e98e9c07a368d23eac
Ben Glassman <ben@vtdesignworks.com>
2014-08-16 19:31:12 -0400

    Refactor #4 to use guess_string_single_char_xor

--- set1/4-detect-single-char-xor.py
+++ set1/4-detect-single-char-xor.py
@@ -1,19 +1,17 @@
 import support
 from os import path
+import sys
 
 DATAFILE_PATH = path.join(path.dirname(path.realpath(__file__)), 'data', '4.txt')
-SCORE_THRESHOLD = 100
 chars = support.get_common_ascii_chars()
 
 results = []
 with open(DATAFILE_PATH) as file:
     for line in file.read().splitlines():
-        decoded = support.hex2ascii(line)
-        for c in chars:
-            result = support.string_single_char_xor(decoded, c)
-            score = support.calculate_english_language_probability_score(result)
-            if score >= SCORE_THRESHOLD:
-                results.append((c, score, result))
-        results_sorted = sorted(results, key=lambda result: result[1], reverse=True)
-
-    print results_sorted
+        guess = support.guess_string_single_char_xor(
+                support.hex2ascii(line),
+                chars
+            )[:1]
+        if guess:
+            print guess
+            sys.exit()
