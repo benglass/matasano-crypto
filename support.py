b553b5b3f379c7e14c9cf76fdaf4ef1a8e8be4f6
Ben Glassman <ben@vtdesignworks.com>
2014-08-16 18:59:00 -0400

    Move english language likelihood score function into support file.

--- support.py
+++ support.py
@@ -28,3 +28,46 @@ def string_single_char_xor(string, char):
     stringbytes = bytearray(string)
     xord = [ chr(b ^ ord(char)) for b in stringbytes ]
     return ''.join(xord)
+
+CHAR_FREQUENCY_WEIGHTS = [
+    ('e',13.0001),
+    ('t',9.056),
+    ('a',8.167),
+    ('o',7.507),
+    ('i',6.966),
+    ('n',6.749),
+    ('s',6.327),
+    ('h',6.094),
+    ('r',5.987),
+    ('d',4.253),
+    ('l',4.025),
+    ('c',2.782),
+    ('u',2.758),
+    ('m',2.406),
+    ('w',2.360),
+    ('f',2.228),
+    ('g',2.015),
+    ('y',1.974),
+    ('p',1.929),
+    ('b',1.492),
+    ('v',0.978),
+    ('k',0.772),
+    ('j',0.153),
+    ('x',0.150),
+    ('q',0.095),
+    ('z',0.074)
+]
+
+def score_string_by_char_freq(string, weights):
+    score = 0
+    for x in weights:
+        char, weight = x
+        score += weight * string.count(char)
+
+    return score
+
+def calculate_english_language_probability_score(string):
+    if string.count(' ') == 0:
+        return 0
+
+    return score_string_by_char_freq(string, CHAR_FREQUENCY_WEIGHTS)
